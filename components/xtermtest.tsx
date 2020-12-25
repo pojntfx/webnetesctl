0f6512ada13ded1d961c72ed6ec0dde2fc3833b1
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-25 13:10:01 +0000

    fix: Correct typo in xtermtest for checking if on server or client

--- components/xtermtest.tsx
+++ components/xtermtest.tsx
@@ -1,6 +1,6 @@
 import { XTerm } from "xterm-for-react";
 
 const XTermTest = ({ forwardRef }: { forwardRef: any }) =>
-  typeof window === undefined ? <div /> : <XTerm ref={forwardRef} />;
+  typeof window === "undefined" ? <div /> : <XTerm ref={forwardRef} />;
 
 export default XTermTest;
