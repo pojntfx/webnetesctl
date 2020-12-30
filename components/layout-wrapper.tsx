6a99bde2cccfb16e2a76c19e162d9443c7bcaec3
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-30 00:14:49 +0000

    feat: Make left/right margins consistent on mobile

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import LayoutTmpl from "antd/lib/layout/layout";
 
 export const Wrapper = styled.div`
-  margin-top: 64px;
-  margin-left: 50px;
-  margin-right: 50px;
   padding-top: 1rem;
   padding-bottom: 1rem;
+  margin: 12px 16px;
+  margin-top: 64px;
 
   @media screen and (min-width: 812px) {
+    margin-left: 50px;
+    margin-right: 50px;
     margin-bottom: 64px;
   }
 `;
