c536fa88f8880c21f825bc3de364a9656a0c2bb1
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-31 03:07:38 +0000

    feat: Add version info to config page

--- pages/config.tsx
+++ pages/config.tsx
@@ -1,10 +1,12 @@
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Text from "antd/lib/typography/Text";
 import Animate from "rc-animate";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { Wrapper } from "../components/layout-wrapper";
 import glass from "../styles/glass";
+import packageJSON from "../package.json";
 
 function Config() {
   const { t } = useTranslation();
@@ -18,13 +20,24 @@ function Config() {
             {t("connected")}!
           </ConnectedStatus>
 
-          <div>
-            <h2>127.0.0.10</h2>
-          </div>
+          <ClusterData>
+            <IPAddress>127.0.0.10</IPAddress>
+          </ClusterData>
 
-          <div>
-            <h2>webnetes 0.0.5</h2>
-          </div>
+          <VersionInformation>
+            <dl>
+              <dt>webnetesctl</dt>
+              <dd>
+                <Text code>{packageJSON.version}</Text>
+              </dd>
+              <dt>webnetes</dt>
+              <dd>
+                <Text code>
+                  {packageJSON.dependencies["@pojntfx/webnetes"]}
+                </Text>
+              </dd>
+            </dl>
+          </VersionInformation>
         </StatusCard>
       </Wrapper>
     </Animate>
@@ -47,6 +60,23 @@ const StatusCard = styled.div`
 
 const ConnectedStatus = styled.div`
   font-size: 1.5rem;
+`;
+
+const ClusterData = styled.div``;
+
+const IPAddress = styled.span`
+  font-size: 2.5rem;
+`;
+
+const VersionInformation = styled.div`
+  dl,
+  dd:last-child {
+    margin-bottom: 0;
+  }
+
+  code {
+    margin: 0;
+  }
 `;
 
 export default Config;
