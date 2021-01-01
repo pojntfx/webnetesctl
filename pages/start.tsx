360fc2336f4837d74c7afc05e721887d7332c3d7
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-01-01 21:58:55 +0000

    refactor: Use centered wrapper for start page

--- pages/start.tsx
+++ pages/start.tsx
@@ -1,6 +1,6 @@
 import Animate from "rc-animate";
 import { useTranslation } from "react-i18next";
-import { Wrapper } from "../components/layout-wrapper";
+import styled from "styled-components";
 
 function Start() {
   const { t } = useTranslation();
@@ -8,10 +8,23 @@ function Start() {
   return (
     <Animate transitionName="fadeandzoom" transitionAppear>
       <Wrapper>
-        <img alt={t("webnetesLogo")} src="/logo.svg" />
+        <Image alt={t("webnetesLogo")} src="/logo.svg" />
       </Wrapper>
     </Animate>
   );
 }
+
+export const Wrapper = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 25rem;
+`;
 
 export default Start;
